299ddedfd590690e78f0e0003cfa967898b45578
drmeloy <drmeloy@gmail.com>
2019-10-09 16:13:32 -0600

    preparing for updated master

--- test/instrument-test.js
+++ test/instrument-test.js
@@ -1,4 +1,5 @@
-import renderInstrument from '../src/render-instrument.js';
+import renderInstrument from '../shop/render-instrument.js';
+import renderTableRow from '../shopping-cart/render-table-row.js';
 
 const test = QUnit.test;
 
@@ -19,6 +20,32 @@ test('renderInstrument correctly renders an instrument', assert => {
     // act
     const dom = renderInstrument(guitar);
     const html = dom.outerHTML;
+    
+    // assert
+    assert.equal(html, expected);
+});
+
+test('renderTableRow correctly renders table row', assert => {
+    // arrange
+    const guitar = {
+        id: 'guitar',
+        brand: 'Guild',
+        image: '../img/guild.jpg',
+        description: 'A good guitar',
+        category: 'stringed',
+        price: 500,
+    };
+
+    const cart = {
+        code: 'apple',
+        quantity: 4
+    };
+
+    const expected = '<tr><td>apple</td><td>4</td><td>$1.00</td><td>$4.00</td></tr>';
+    
+    // act
+    const fruitElementTr = renderTableRow(apple, appleOrder);
+    const html = fruitElementTr.outerHTML;
     
     // assert
     assert.equal(html, expected);
